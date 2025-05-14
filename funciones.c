29830fd9a706d22119c7c4fe3b85865e45c9868f
Nickolas Landeta <nickolas.landeta@udla.edu.ec>
2025-05-14 18:47:29 -0500

    avance

--- funciones.c
+++ funciones.c
@@ -32,27 +32,55 @@ int menu(){
     return opc;
 }
 
-int IngresarProductos(char nombres[5][30], int contp) {
+int IngresarProductos(char nombres[5][30], float (*cantidadesPorProducto)[10], char ingredientes[10][30], int contIngredientes, int contp) {
     if (contp < 5) {
         printf("Ingrese el nombre del producto %d: ", contp + 1);
-        fflush(stdin); // Limpiar el buffer de entrada
+        fflush(stdin);
         fgets(nombres[contp], 30, stdin);
 
-        // Elimina el salto de línea al final del nombre
+        // Eliminar salto de línea
         int len = strlen(nombres[contp]) - 1;
         if (nombres[contp][len] == '\n') {
             nombres[contp][len] = '\0';
         }
 
+        printf("¿Cuántos ingredientes necesita '%s'? ", nombres[contp]);
+        int numIng = (int)validarIngreso();
+
+        for (int i = 0; i < numIng; i++) {
+            char nombreIng[30];
+            printf("Ingrese el nombre del ingrediente %d: ", i + 1);
+            fflush(stdin);
+            fgets(nombreIng, 30, stdin);
+            int lenIng = strlen(nombreIng) - 1;
+            if (nombreIng[lenIng] == '\n') nombreIng[lenIng] = '\0';
+
+            // Buscar el ingrediente en la lista global
+            int encontrado = -1;
+            for (int j = 0; j < contIngredientes; j++) {
+                if (strcmp(nombreIng, ingredientes[j]) == 0) {
+                    encontrado = j;
+                    break;
+                }
+            }
+
+            if (encontrado != -1) {
+                printf("Ingrese la cantidad necesaria de %s: ", nombreIng);
+                float cantidad = validarIngreso();
+                *(*(cantidadesPorProducto + contp) + encontrado) = cantidad;
+            } else {
+                printf("Ingrediente '%s' no encontrado. Debe registrarlo primero.\n", nombreIng);
+            }
+        }
 
         contp++;
-
     } else {
         printf("No se pueden ingresar más productos. El límite es 5.\n");
     }
 
     return contp;
 }
+
 
 void RegistrarIngredientes(char ingredientes[10][30], float cantidades[10], int *contador) {
     if (*contador < 10) {
@@ -74,3 +102,56 @@ void RegistrarIngredientes(char ingredientes[10][30], float cantidades[10], int 
         printf("No se pueden ingresar más ingredientes. Límite alcanzado.\n");
     }
 }
+
+void RealizarPedido(char ingredientes[10][30], float cantidades[10], int contador) {
+    if (contador == 0) {
+        printf("No hay ingredientes registrados para realizar un pedido.\n");
+        return;
+    }
+
+    printf("\n--- Ingredientes disponibles ---\n");
+    for (int i = 0; i < contador; i++) {
+        printf("%d. %s - %.2f unidades\n", i + 1, ingredientes[i], cantidades[i]);
+    }
+
+    char nombre[30];
+    float cantidadSolicitada;
+    char continuar;
+
+    do {
+        printf("\nIngrese el nombre del ingrediente que desea usar: ");
+        fflush(stdin);
+        fgets(nombre, 30, stdin);
+        int len = strlen(nombre) - 1;
+        if (nombre[len] == '\n') {
+            nombre[len] = '\0';
+        }
+
+        int encontrado = 0;
+        for (int i = 0; i < contador; i++) {
+            if (strcmp(nombre, ingredientes[i]) == 0) {
+                encontrado = 1;
+                printf("Ingrese la cantidad que desea usar de %s: ", nombre);
+                cantidadSolicitada = validarIngreso();
+
+                if (cantidadSolicitada <= cantidades[i]) {
+                    cantidades[i] -= cantidadSolicitada;
+                    printf("Pedido realizado: %.2f unidades de %s\n", cantidadSolicitada, nombre);
+                } else {
+                    printf("No hay suficiente cantidad disponible. Solo hay %.2f unidades.\n", cantidades[i]);
+                }
+                break;
+            }
+        }
+
+        if (!encontrado) {
+            printf("Ingrediente no encontrado.\n");
+        }
+
+        printf("¿Desea pedir otro ingrediente? (s/n): ");
+        fflush(stdin);
+        scanf(" %c", &continuar);
+
+    } while (continuar == 's' || continuar == 'S');
+}
+
--- main.c
+++ main.c
@@ -1,28 +1,43 @@
 #include <stdio.h>
 #include "funciones.h"
-
-int main (int argc, char *argv[]) {
-    char nombres[5][30];
+#include <string.h>
+int main() {
+    char productos[5][30];
     char ingredientes[10][30];
     float cantidades[10];
-    int opc;
-    int contp=0,conti=0;
-    do
-    {
-        opc= menu();
-        switch (opc)
-        {
-        case 1:
-            contp=IngresarProductos(nombres,contp);
-            break;
-        case 2:
-            RegistrarIngredientes(ingredientes,cantidades,conti);
-        
-        default:
-            break;
+    int contProductos = 0;
+    int contIngredientes = 0;
+    int opcion;
+
+    do {
+        opcion = menu();
+
+        switch (opcion) {
+            case 1:
+                contProductos = IngresarProductos(productos, contProductos);
+                break;
+            case 2:
+                RegistrarIngredientes(ingredientes, cantidades, &contIngredientes);
+                break;
+            case 3:
+                printf("Función para ingresar pedido aún no implementada.\n");
+                break;
+            case 4:
+                printf("Función para calcular precio promedio aún no implementada.\n");
+                break;
+            case 5:
+                printf("Función de búsqueda de producto aún no implementada.\n");
+                break;
+            case 6:
+                printf("Saliendo del programa...\n");
+                break;
+            default:
+                printf("Opción no válida. Intente nuevamente.\n");
         }
-    } while (opc !=6);
-    
+
+        printf("\n");
+
+    } while (opcion != 6);
 
     return 0;
 }
