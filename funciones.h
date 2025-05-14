cee00fe9355af6666e76706aa1a6b63e66d0d8e6
Nickolas Landeta <nickolas.landeta@udla.edu.ec>
2025-05-14 18:50:40 -0500

    xd

--- funciones.h
+++ funciones.h
@@ -1,4 +1,4 @@
 float validarIngreso();
 int menu();
-int IngresarProductos(char nombres[][30], int contp);
+int IngresarProductos(char nombres[][30], float (*cantidadesPorProducto)[10], char ingredientes[][30], int contIngredientes, int contp);
 void RegistrarIngredientes(char ingredientes[][30], float cantidades[10], int *contador);
--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
-#include "funciones.h"
 #include <string.h>
+#include "funciones.h"
+
 int main() {
     char productos[5][30];
     char ingredientes[10][30];
     float cantidades[10];
+    float cantidadesPorProducto[5][10] = {0}; 
     int contProductos = 0;
     int contIngredientes = 0;
     int opcion;
@@ -14,16 +16,16 @@ int main() {
 
         switch (opcion) {
             case 1:
-                contProductos = IngresarProductos(productos, contProductos);
+                contProductos = IngresarProductos(productos, cantidadesPorProducto, ingredientes, contIngredientes, contProductos);
                 break;
             case 2:
                 RegistrarIngredientes(ingredientes, cantidades, &contIngredientes);
                 break;
             case 3:
-                printf("Función para ingresar pedido aún no implementada.\n");
+                RealizarPedido(ingredientes, cantidades, contIngredientes);
                 break;
             case 4:
-                printf("Función para calcular precio promedio aún no implementada.\n");
+                printf("Función de precio promedio aún no implementada.\n");
                 break;
             case 5:
                 printf("Función de búsqueda de producto aún no implementada.\n");
