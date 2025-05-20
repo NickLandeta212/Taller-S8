9ce9349a8a70cf9710046ad904826eaa2ec84ca6
Nickolas Landeta <nickolas.landeta@udla.edu.ec>
2025-05-20 14:35:21 -0500

    xd

--- main.c
+++ main.c
@@ -1,50 +1,44 @@
 #include <stdio.h>
-#include <string.h>
 #include "funciones.h"
 
 int main() {
     char ingredientes[10][30];
-    float cantidades[10];
-    int contIngredientes = 0;
-
-    char nombres[5][30];
-    float cantidadesPorProducto[5][10] = {0};
-    int contp = 0;
-
-    float tiemposProduccion[5] = {0};
+    float cantidades[10] = {0};
     float stock[10] = {0};
+    int contadorIngredientes = 0;
+
+    char productos[5][30];
+    float cantidadesPorProducto[5][10] = {{0}};
+    float tiemposProduccion[5] = {0};
+    int contadorProductos = 0;
 
     int opcion;
-
     do {
         opcion = menu();
         switch (opcion) {
             case 1:
-                RegistrarIngredientes(ingredientes, cantidades, &contIngredientes);
+                RegistrarIngredientes(ingredientes, cantidades, &contadorIngredientes);
+                for (int i = 0; i < contadorIngredientes; i++) {
+                    stock[i] = cantidades[i]; // Inicializa el stock con las cantidades ingresadas
+                }
                 break;
             case 2:
-                contp = IngresarProductos(nombres, cantidadesPorProducto, ingredientes, contIngredientes, contp);
+                contadorProductos = IngresarProductos(productos, cantidadesPorProducto, ingredientes, contadorIngredientes, contadorProductos);
                 break;
             case 3:
-                RegistrarTiempoProduccion(nombres, tiemposProduccion, contp);
+                RegistrarTiempoProduccion(productos, tiemposProduccion, contadorProductos);
                 break;
             case 4:
-                RealizarPedido(nombres, cantidadesPorProducto, ingredientes, stock, tiemposProduccion, contp, contIngredientes);
+                RealizarPedido(productos, cantidadesPorProducto, ingredientes, stock, tiemposProduccion, contadorProductos, contadorIngredientes);
                 break;
             case 5:
-                EditarProducto(nombres, cantidadesPorProducto, contp);
-                break;
-            case 6:
-                EliminarProducto(nombres, cantidadesPorProducto, &contp);
-                break;
-            case 7:
-                printf("Saliendo del programa.\n");
+                printf("Saliendo del programa...\n");
                 break;
             default:
-                printf("Opcion invalida. Intente nuevamente.\n");
-                break;
+                printf("Opción no válida. Intente nuevamente.\n");
         }
-    } while (opcion != 7);
+    } while (opcion != 5);
 
     return 0;
 }
+
